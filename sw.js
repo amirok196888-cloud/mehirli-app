168270f80becdf101569533648b6db4130c3559b
amirok196888-cloud <ami.rok196888@gmail.com>
2026-08-30 14:22:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mehirli-v17';
+const CACHE='mehirli-v18';
 const ASSETS=['./','./index.html','./style.css?v=16','./app.js?v=16','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./share-preview-v16.jpg','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
