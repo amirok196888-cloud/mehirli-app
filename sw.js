e79857a498be97be54d00828655760937d9504f7
amirok196888-cloud <ami.rok196888@gmail.com>
2026-09-15 21:50:27 +0300

    Refresh cache for pricing calculation fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mehirli-v45';
-const ASSETS=['./','./index.html','./style.css?v=43','./app.js?v=44','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./share-preview-v20.jpg','./mehirli-hero-v43.jpg','./share.html'];
+const CACHE='mehirli-v46';
+const ASSETS=['./','./index.html','./style.css?v=43','./app.js?v=45','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./share-preview-v20.jpg','./mehirli-hero-v43.jpg','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
