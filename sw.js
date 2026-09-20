8912e3459bb8476088d75b67d9f1f0ce5ca6470c
amirok196888-cloud <ami.rok196888@gmail.com>
2026-09-20 14:33:22 +0300

    Upload complete mobile-safe quote tutorial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mehirli-v79';
+const CACHE='mehirli-v80';
 const ASSETS=['./','./index.html','./landing.css?v=66','./mehirli-explainer-poster-v62.jpg','./app.html','./style.css?v=77','./app.js?v=77','./sample-quote.html','./quote.html','./quote-preview-v61.jpg','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./share-preview-v20.jpg','./mehirli-hero-v43.jpg','./share.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
